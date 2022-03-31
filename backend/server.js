e7e22ad59e755bea9883278726dbb3f8213e089c
Rishaba Priyan <rishabapriyan@gmail.com>
2022-03-31 17:48:51 +0530

    Added first route

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('Aarambikalam gala...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/', (req, res) => {
+	res.status(200).json({ message: 'vandhutaaya! vandhutaaya!' })
+})
+app.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`)
+})
