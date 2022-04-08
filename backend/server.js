4ef60f3261633257601636dc9e2e0521775c3526
Rishaba Priyan <rishabapriyan@gmail.com>
2022-04-08 21:18:30 +0530

    Final changes

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,38 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const colors = require('colors')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5000
 
-//connect to DB
+// Connect to database
 connectDB()
+
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-	res.status(200).json({ message: 'vandhutaaya! vandhutaaya!' })
-})
-
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
-// serve front end
+
+// Serve Frontend
 if (process.env.NODE_ENV === 'production') {
-	//set build folder as static
+	// Set build folder as static
 	app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-	app.get('*', (req, res) =>
-		res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
-	)
+	// FIX: below code fixes app crashing on refresh in deployment
+	app.get('*', (_, res) => {
+		res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+	})
 } else {
 	app.get('/', (req, res) => {
 		res.status(200).json({ message: 'Welcome to the Support Desk API' })
 	})
 }
+
 app.use(errorHandler)
 
-app.listen(PORT, () => {
-	console.log(`Server started on port ${PORT}`)
-})
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
